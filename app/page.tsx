86112d2a6f068f7bf8ecf2bdd040ea384e43c460
Bartosz Klimowicz <klimowicz.bartek@gmail.com>
2024-04-27 02:28:41 +0200

    Cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Header from './components/header/header';
 import QuickActionsPanel from './components/quick-actions-panel/quick-actions-panel';
 import ResultPanel from './components/result-panel/result-panel';
-import SearchBar from './components/search-bar/search-bar';
 
 export default function Home() {
     //const items: Card[] = [];
